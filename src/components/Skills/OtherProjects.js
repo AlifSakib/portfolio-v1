956dded004916becc73857e5d369129f649f8269
AlifSakib <alifsakib@gmail.com>
2022-12-08 06:38:02 +0600

    added projects details

--- src/components/Skills/OtherProjects.js
+++ src/components/Skills/OtherProjects.js
@@ -43,25 +43,17 @@ const OtherProjects = () => {
             <div className="space-y-2">
               <h1 className="font-bold text-lg text-blue-600">Learning Hub</h1>
               <p className="text-sm text-[#112D4E]">
-                An simple online quiz website where user can select topic and
-                start quiz and for right answer a tost will show and give him
-                alert that answer is right if wrong then wrong .
+                An Online learning platform build with bootstrap
               </p>
               <div className="flex flex-wrap text-[#112D4E]">
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
-                  <p className="px-4">React</p>
+                  <p className="px-4">Html</p>
                 </div>
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
-                  <p className="px-4">React Router Dom</p>
+                  <p className="px-4">Bootstrap</p>
                 </div>
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
-                  <p className="px-4">Tailwind</p>
-                </div>
-                <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
-                  <p className="px-4">Firebase</p>
-                </div>
-                <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
-                  <p className="px-4">Private Routes</p>
+                  <p className="px-4">esponsive</p>
                 </div>
               </div>
             </div>
@@ -72,9 +64,8 @@ const OtherProjects = () => {
                 Beyond Horizon
               </h1>
               <p className="text-sm text-[#112D4E]">
-                An simple online quiz website where user can select topic and
-                start quiz and for right answer a tost will show and give him
-                alert that answer is right if wrong then wrong .
+                An travle booking website build with react , tailwind ,
+                react-router-dom .
               </p>
               <div className="flex flex-wrap text-[#112D4E]">
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
@@ -99,9 +90,9 @@ const OtherProjects = () => {
             <div className="space-y-2">
               <h1 className="font-bold text-lg text-blue-600">Hank's Garage</h1>
               <p className="text-sm text-[#112D4E]">
-                An simple online quiz website where user can select topic and
-                start quiz and for right answer a tost will show and give him
-                alert that answer is right if wrong then wrong .
+                An online car repair platform build with react , tailwind, node
+                , express , mongodb . Implemented pagination , User
+                Authentication , Jwt authorization .
               </p>
               <div className="flex flex-wrap text-[#112D4E]">
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
@@ -118,6 +109,12 @@ const OtherProjects = () => {
                 </div>
                 <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
                   <p className="px-4">Private Routes</p>
+                </div>
+                <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
+                  <p className="px-4">Node</p>
+                </div>
+                <div className="border py-px border-[#112D4E] rounded-md text-sm mr-4 mb-4">
+                  <p className="px-4">Express</p>
                 </div>
               </div>
             </div>
